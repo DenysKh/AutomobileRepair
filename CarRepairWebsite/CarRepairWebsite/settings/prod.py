2e6a6294848fa773675585688b4371185f33dee2
DenysKh <denyskh.sool@gmail.com>
2023-05-19 19:43:16 +0300

    fix host

--- CarRepairWebsite/CarRepairWebsite/settings/prod.py
+++ CarRepairWebsite/CarRepairWebsite/settings/prod.py
@@ -18,7 +18,7 @@ ADMINS = (('John', 'john@example.com'), )
 SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-+5^5j751%lu20!#%t3cx70td^8v4^l^j=9tm+$hr(em=@=sks)')
 
 # https://docs.djangoproject.com/en/3.2/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['.example.com', '*']
+ALLOWED_HOSTS = ["*"]
 
 # You can change this to something like 'MyForum <noreply@example.com>'
 DEFAULT_FROM_EMAIL = 'webmaster@localhost'  # Django default
